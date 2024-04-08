0aca93183ddfaacd4ecf57ab4e68966ff6ee855e
Fillipi <pfillipi@gmx.de>
2024-04-08 00:47:12 +0000

    Nye oppdateringer.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <h1>Olá, tudo bem?</h1>
-    return <h1>O clima está ficando um pouco mais quente.</h1>
-    return <h1>Creio que hoje foi o dia mais quente deste que começou a primavera e chegamos a 12 graus.</h1>
+    return <h2>O clima está ficando um pouco mais quente.</h2>
+    return <h3>Creio que hoje foi o dia mais quente deste que começou a primavera e chegamos a 12 graus.</h3>
 }
 
 export default Home;
