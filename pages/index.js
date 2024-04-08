1c9802812c13699883732f9c2080cc6237df7491
Fillipi <pfillipi@gmx.de>
2024-04-08 00:31:13 +0000

    Melding

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Ny meldinger</h1>
+    return <h1>Olá, tudo bem?</h1>
+    return <h1>O clima está ficando um pouco mais quente.</h1>
+    return <h1>Creio que hoje foi o dia mais quente deste que começou a primavera e chegamos a 12 graus.</h1>
 }
 
 export default Home;
