f081f59ab2baba6ec592a13b52fe785bfe7b79f9
Fillipi <pfillipi@gmx.de>
2024-04-08 01:17:11 +0000

    Meldinger

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return <h1>Olá, tudo bem?</h1>
-    return <h2>O clima está ficando um pouco mais quente.</h2>
-    return <h3>Creio que hoje foi o dia mais quente deste que começou a primavera e chegamos a 12 graus.</h3>
+    return <h1>Hoje nos apresentamos na reunião mensal do distrito tocando duas músicas do Tom Jobim.</h1>
 }
 
 export default Home;
