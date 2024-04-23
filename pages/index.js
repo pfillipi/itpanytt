26575dd7e5f34a33eba1672d4073bfccbb5cd334
Fillipi <pfillipi@gmx.de>
2024-04-23 13:05:24 +0000

    Papai.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Olá querida mamãe!<p>Muito obrigado pela transferência!</p></h1>
+    return <h1>Olá querido papai!
+                <p>Como está o seu dia hoje?</p>
+                <p>Espero que a página esteja funcionando hoje.</p>
+                <p>Hoje o sol está começando a brilhar um pouco, talvez menos do que hoje.</p>
+            </h1>
 }
 
 export default Home;
