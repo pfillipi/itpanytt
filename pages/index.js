e2fdf1523b8a466de483fc468fe267cf8dfda51c
Fillipi <pfillipi@gmx.de>
2024-04-23 13:07:20 +0000

    Papai.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home() {
     return <h1>Olá querido papai!
                 <p>Como está o seu dia hoje?</p>
-                <p>Espero que a página esteja funcionando hoje.</p>
-                <p>Hoje o sol está começando a brilhar um pouco, talvez menos do que hoje.</p>
+                <p>Hoje o sol está começando a brilhar um pouco, talvez menos do que ontem.</p>
             </h1>
 }
 
