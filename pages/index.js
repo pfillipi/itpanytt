7d507aabe009a84c936fdabbedb35aab963572b6
Fillipi <pfillipi@gmx.de>
2024-05-19 18:37:35 +0000

    legg til `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ function Home() {
   return (
     <h1>
       Nettstedet er under oppbygging.
-      <p>Vi vil gi deg beskjed så snart vi har en oppdatering klar for vår nettapp.</p>
       <p>
-      Vi er midtveis i prosessen innen milepælene.
+        Vi vil gi deg beskjed så snart vi har en oppdatering klar for vår
+        nettapp.
       </p>
+      <p>Vi er midtveis i prosessen innen milepælene.</p>
     </h1>
   );
 }
