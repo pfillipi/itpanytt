16334c2f7b6df4632b91dc6abba12c7ec7699c45
Fillipi <pfillipi@gmx.de>
2024-07-15 01:13:08 +0000

    add /api/v1/status` endpoint

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: [\n            \"Nettstedet er under oppbygging.\",\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"p\", {\n                children: \"Vi vil gi deg beskjed s\\xe5 snart vi har en oppdatering klar for v\\xe5r nettapp.\"\n            }, void 0, false, {\n                fileName: \"/workspaces/tabnyheter/pages/index.js\",\n                lineNumber: 5,\n                columnNumber: 7\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"p\", {\n                children: \"Vi er midtveis i prosessen innen milep\\xe6lene.\"\n            }, void 0, false, {\n                fileName: \"/workspaces/tabnyheter/pages/index.js\",\n                lineNumber: 9,\n                columnNumber: 7\n            }, this)\n        ]\n    }, void 0, true, {\n        fileName: \"/workspaces/tabnyheter/pages/index.js\",\n        lineNumber: 3,\n        columnNumber: 5\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsU0FBU0EsT0FBTztJQUNkLHFCQUNFLDhEQUFDQzs7WUFBRzswQkFFRiw4REFBQ0M7MEJBQUU7Ozs7OzswQkFJSCw4REFBQ0E7MEJBQUU7Ozs7Ozs7Ozs7OztBQUdUO0FBRUEsaUVBQWVGLElBQUlBLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90YWJueWhldGVyLy4vcGFnZXMvaW5kZXguanM/YmVlNyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBIb21lKCkge1xuICByZXR1cm4gKFxuICAgIDxoMT5cbiAgICAgIE5ldHRzdGVkZXQgZXIgdW5kZXIgb3BwYnlnZ2luZy5cbiAgICAgIDxwPlxuICAgICAgICBWaSB2aWwgZ2kgZGVnIGJlc2tqZWQgc8OlIHNuYXJ0IHZpIGhhciBlbiBvcHBkYXRlcmluZyBrbGFyIGZvciB2w6VyXG4gICAgICAgIG5ldHRhcHAuXG4gICAgICA8L3A+XG4gICAgICA8cD5WaSBlciBtaWR0dmVpcyBpIHByb3Nlc3NlbiBpbm5lbiBtaWxlcMOmbGVuZS48L3A+XG4gICAgPC9oMT5cbiAgKTtcbn1cblxuZXhwb3J0IGRlZmF1bHQgSG9tZTtcbiJdLCJuYW1lcyI6WyJIb21lIiwiaDEiLCJwIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
--- models/kalkulator.js
+++
@@ -1,8 +0,0 @@
-function leggtil(nummer1, nummer2) {
-  if (typeof nummer1 !== "number") {
-    return "feil";
-  }
-  return nummer1 + nummer2;
-}
-
-exports.leggtil = leggtil;
