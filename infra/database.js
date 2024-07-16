0325376f2feb9f17ddc07c889da304a4e1525525
Fillipi <pfillipi@gmx.de>
2024-07-16 21:59:36 +0000

    query 2

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client();
+  const client = new Client({
+    host: "localhost",
+  });
   await client.connect();
   await client.query(queryObject);
   const result = await client.query(queryObject);
