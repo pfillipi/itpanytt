fa5eda7fcd3e67cdce0957582e9c663652fc14de
Fillipi <pfillipi@gmx.de>
2024-07-18 20:36:38 +0000

    Environment

--- infra/database.js
+++ infra/database.js
@@ -3,6 +3,10 @@ import { Client } from "pg";
 async function query(queryObject) {
   const client = new Client({
     host: "localhost",
+    port: 5432,
+    user: "postgres",
+    database: "postgres",
+    password: process.env.POSTGRESS_PASSWORD,
   });
   await client.connect();
   await client.query(queryObject);
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({
     nøkkel: "er mennesker over gjennomsnittet",
   });
