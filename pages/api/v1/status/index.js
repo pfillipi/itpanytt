869beda53924cba22ff31b67fc67edef46dbf52c
Fillipi <pfillipi@gmx.de>
2025-04-26 23:18:58 +0000

    Oppdater innen tester

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({
-    nøkkel: "er mennesker over gjennomsnittet",
-  });
+  response.status(200).json({});
 }
 
 export default status;
