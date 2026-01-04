216487b498eb81709fcf9a65cbdc1f6cb70931ac
Fillipi <pfillipi@gmx.de>
2026-01-04 04:41:13 +0000

    Show max_connections;

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,22 +6,9 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  // const dbVersionResult = await database.query("SHOW server_version;");
-  // const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
-  // const dbUsedConnectionsResult = await database.query(`
-  //   SELECT COUNT(*)::int
-  //   FROM pg_stat_activity
-  //   WHERE state = 'active';
-  //   `);
-
-  // const dbInfo = {
-  //   version: dbVersionResult.rows[0].server_version,
-  //   max_connections: parseInt(
-  //     dbMaxConnectionsResult.rows[0].max_connections,
-  //     10,
-  //   ),
-  //   used_connections: dbUsedConnectionsResult.rows[0].count,
-  // };
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
 
   response.status(200).json({
     updated_at: updatedAt,
