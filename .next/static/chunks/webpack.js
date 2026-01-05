b780eaba052a62493ae074c846a85c65547b875d
Fillipi <pfillipi@gmx.de>
2026-01-05 22:13:39 +0000

    OpendConnectionsResult

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eda3ea192a301264"; }
+/******/ 		__webpack_require__.h = function() { return "4c8be056d9ab14f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -12,6 +12,11 @@ async function status(request, response) {
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity;",
+  );
+  console.log(databaseOpenedConnectionsResult.rows);
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
