468482fa688883e4c0c7705424768f1dd11f4137
Fillipi <pfillipi@gmx.de>
2026-02-03 02:28:35 +0000

    Postgresqll-legitimasjon

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,13 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log("PostgreSQL-legitimasjon:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   await client.connect();
 
   try {
