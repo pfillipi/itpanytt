59a84ed4487a2c761f3aada67374c23bf3984442
Fillipi <pfillipi@gmx.de>
2026-03-25 11:00:00 +0000

    -env

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("PostgreSQL-legitimasjon:", {
     host: process.env.POSTGRES_HOST,
@@ -24,7 +25,7 @@ async function query(queryObject) {
     console.error(error);
     throw error;
   } finally {
-    //    await client.end();
+    await client.end();
   }
 }
 
